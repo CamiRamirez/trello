1589be45f9eb39553dcaec4fd30cd51029340919
camila <camiramirezpp@gmail.com>
2018-08-14 08:54:58 -0300

    listas suman desde arriba, orden ok

--- src/js/view.js
+++ src/js/view.js
@@ -28,8 +28,8 @@ window.addNewList = (id) => {
     const newTextList = document.getElementById('listCard');
     let id = document.getElementsByClassName('numDivs').length;
     newTextList.innerHTML += `
-    <div id="listBox" class="form-inline numDivs">
-      <div id="listText"><strong>${textList}</strong></div>
+    <div id="listBox${id}" class="d-inline-block numDivs listBox">
+      <div id="listText"><strong id="breakWord">${textList}</strong></div>
       <button id="btnNewCard${id}" class="btnNewCard" onclick="addNewCard(${id})"><i class="fas fa-plus" id="iconaddCard"></i><h2 id="titleAddList2"> Añada una tarjeta</h2></button>
       <div id="containerCards${id}">
         <div id="listTextCard${id}"></div>
@@ -39,15 +39,16 @@ window.addNewList = (id) => {
   }
 }
 
-//FUNCION PARA AGREGAR NUEVA TARJETA DENTRO DE LISTA
+//FUNCION PARA AÑADIR NUEVA TARJETA DENTRO DE LISTA
 window.addNewCard = (id) => {
   const container = document.getElementById('containerCards' + id);
-  document.getElementById('btnNewCard' + id).style.display = 'none';
+  document.getElementById('btnNewCard'+id).style.display = 'none';
   container.innerHTML += `
     <textarea id="nameCard${id}" class="nameCard" placeholder=" Introduzca un título para esta tarjeta..."></textarea>
     <button class="btn btn-info" id="btnAddNewCard${id}" onclick="addCard(${id})"><strong>Añadir tarjeta</strong></button>
     <button id="closeBtn2${id}" class="closeBtn2" onclick="closeAddCard(${id})"><i class="fas fa-times"></i></button>
   `;
+  
 }
 
 //FUNCION QUE IMPRIME TARJETA EN LISTA
@@ -72,7 +73,8 @@ window.closeAddCard = (id) => {
   document.getElementById('closeBtn2' + id).style.display = 'none';
   document.getElementById('nameCard' + id).style.display = 'none';
     container.innerHTML += `
-      <button id="btnNewCard" class="btnNewCard" onclick="addNewCard(${id})"><i class="fas fa-plus" id="iconaddCard"></i><h2 id="titleAddList2"> Añada una tarjeta</h2></button>
+    <button id="btnNewCard${id}" class="btnNewCard" onclick="addNewCard(${id})"><i class="fas fa-plus" id="iconaddCard"></i><h2 id="titleAddList2"> Añada una tarjeta</h2></button>
     `;
+    
 }
 
